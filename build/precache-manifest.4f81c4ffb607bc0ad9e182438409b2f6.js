871b6501cb15272f7977e406ac2ea500edc49a24
Mohamed <mohamed123fathiii@gmail.com>
2025-03-26 00:14:18 +0400

    forced rebuild

---
+++ build/precache-manifest.4f81c4ffb607bc0ad9e182438409b2f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc8e10940df36d07e9f36fdfae7b5e6",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "b4598374b932983f1c8d",
+    "url": "/my-portfolio/static/css/2.ee6fb1dc.chunk.css"
+  },
+  {
+    "revision": "85170b3e3213a9ba51b0",
+    "url": "/my-portfolio/static/css/main.7a489aa9.chunk.css"
+  },
+  {
+    "revision": "b4598374b932983f1c8d",
+    "url": "/my-portfolio/static/js/2.6a8e69ff.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/my-portfolio/static/js/2.6a8e69ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85170b3e3213a9ba51b0",
+    "url": "/my-portfolio/static/js/main.17a54465.chunk.js"
+  },
+  {
+    "revision": "10b4c27118e8ba4cf4e2",
+    "url": "/my-portfolio/static/js/runtime-main.5eb7bed6.js"
+  }
+]);
