03bbabe7f27b23f1b4703fbdc5c07e7841366326
Mohamed <mohamed123fathiii@gmail.com>
2025-03-25 21:38:01 +0400

    Updates

---
+++ precache-manifest.0d2101d0447f830e49acd5c1423d43dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d83cf549b37621c39637886f2ffad0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4598374b932983f1c8d",
+    "url": "/static/css/2.ee6fb1dc.chunk.css"
+  },
+  {
+    "revision": "2a090a8d76c1e9d440fb",
+    "url": "/static/css/main.7a489aa9.chunk.css"
+  },
+  {
+    "revision": "b4598374b932983f1c8d",
+    "url": "/static/js/2.6a8e69ff.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/static/js/2.6a8e69ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a090a8d76c1e9d440fb",
+    "url": "/static/js/main.17e38b17.chunk.js"
+  },
+  {
+    "revision": "6074ff583ca711c31fe3",
+    "url": "/static/js/runtime-main.1dbfc099.js"
+  }
+]);
